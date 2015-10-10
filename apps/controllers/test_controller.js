ac9d6788dc24e6e4400e8636de7e4b220caaf9ab
Leon Ho <vuonghominh@gmail.com>
2015-10-10 11:14:36 +0700

    test get data from models

--- apps/controllers/test_controller.js
+++ apps/controllers/test_controller.js
@@ -1,7 +1,18 @@
 'use strict';
 
+var mongoose = require('mongoose'),
+        User = mongoose.model('User');
+
 exports.testApi = function (req, res) {
-  res.json({
-    msg: 'API is running from controller'
+  User.find({}, function(err, users) {
+    var userMap = {};
+
+    users.forEach(function(user) {
+      userMap[user._id] = user;
+    });
+
+    res.json({
+      msg: users
+    });
   });
 };
